7d2d43e6550b0c4cc686df150a453807850e6c23
Cosy-harks <us3rs1@gmail.com>
2018-03-06 15:25:47 -0800

    Kinematics variable++, MoveStep fill in

--- AlephEngine/Kinematics.cpp
+++ AlephEngine/Kinematics.cpp
@@ -1,11 +1,30 @@
 #include "Kinematics.h"
+#include "EngineTime.h"
 
+AlephEngine::Kinematics::Kinematics(Entity * entity) :
+	Component(entity, Component::Type<Kinematics>()),
+	velocity(gmtl::Vec<float, 3>()),
+	rotationalAxis(gmtl::Vec<float, 3>()),
+	centerOfMass(gmtl::Vec<float, 3>()),
+	mass(0.0),
+	angularV(0.0)
+{ }
 
-AlephEngine::Kinematics::Kinematics(Entity * entity)
-	: Component(entity, Component::Type<Kinematics>()) { }
+AlephEngine::Kinematics::Kinematics(Entity * entity, gmtl::Vec<float, 3> vel, gmtl::Vec<float, 3> rotAxis, gmtl::Vec<float, 3> CofM, float M, float AV) :
+	Component(entity, Component::Type<Kinematics>()),
+	velocity(vel),
+	rotationalAxis(rotAxis),
+	centerOfMass(CofM),
+	mass(M),
+	angularV(AV)
+{ }
 
 
 void AlephEngine::Kinematics::MoveStep()
 {
 	// TODO: Fill in code here
+	centerOfMass[0] += velocity[0] * EngineTime::GetDeltaTime();
+	centerOfMass[1] += velocity[1] * EngineTime::GetDeltaTime();
+	centerOfMass[2] += velocity[2] * EngineTime::GetDeltaTime();
+
 }
--- AlephEngine/Kinematics.h
+++ AlephEngine/Kinematics.h
@@ -9,16 +9,20 @@ namespace AlephEngine
 	{
 	public:
 		Kinematics(Entity* entity);
+		Kinematics(Entity* entity, gmtl::Vec<float, 3> vel, gmtl::Vec<float, 3> rotAxis, gmtl::Vec<float, 3> CofM, float M, float AV);
 		
-		// meters/second use like vector x, y, z
+		// meters/second use like vector [index]
 		gmtl::Vec<float, 3> velocity;
+		// Rotational axis
+		gmtl::Vec<float, 3> rotationalAxis;
+		// Relitive UP orthogonal to rotationalAxis
+		gmtl::Vec<float, 3> UP;
 		// meters Collision and gravity epicenters
 		gmtl::Vec<float, 3> centerOfMass;
-		// Kg. used for momentum, collisions, 
+		// Kg. used for momentum, collisions,
 		float mass;
-		
-		static void MoveStep(); // Moves the enitity acording to its physics
+		// angular velocity float
+		float angularV;
+		void MoveStep(); // Moves the enitity acording to its physics
 	};
 }
-
-
