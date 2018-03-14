24c11a6dec3142f16d7ab24541f1d2c7082ad0fd
scottlafetra <scottlafetra@gmail.com>
2018-03-13 22:50:14 -0700

    Revert "Revert "Merge remote-tracking branch 'origin/Kinematics'""
    This reverts commit cc918cc9e92d9b9c93853963b546b03f3f528ba8.

--- AlephEngine/Kinematics.cpp
+++ AlephEngine/Kinematics.cpp
@@ -14,6 +14,24 @@ AlephEngine::Kinematics::Kinematics(Entity * entity)
 	entity->GetScene()->physicsMaster.TrackPhysics(this);
 }
 
+AlephEngine::Kinematics::Kinematics(Entity * entity) :
+	Component(entity, Component::Type<Kinematics>()),
+	velocity(gmtl::Vec<float, 3>()),
+	rotationalAxis(gmtl::Quat<float>()),
+	centerOfMass(gmtl::Vec<float, 3>()),
+	mass(0.0),
+	force(gmtl::Vec<float, 3>())
+{ }
+
+AlephEngine::Kinematics::Kinematics(Entity * entity, gmtl::Vec<float, 3> vel, gmtl::Quat<float> AV, gmtl::Vec<float, 3> CofM, float M, gmtl::Vec<float, 3> F) :
+	Component(entity, Component::Type<Kinematics>()),
+	velocity(vel),
+	rotationalAxis(AV),
+	centerOfMass(CofM),
+	mass(M),
+	force(F)
+{ }
+
 AlephEngine::Kinematics::~Kinematics()
 {
 	entity->GetScene()->physicsMaster.StopTrackingPhysics(this);
@@ -22,4 +40,17 @@ AlephEngine::Kinematics::~Kinematics()
 void AlephEngine::Kinematics::MoveStep()
 {
 	// TODO: Fill in code here
+	//TODO: center of mass should change based on the objects updated position (done by )
+	centerOfMass[0] += velocity[0]*EngineTime::GetDeltaTime();
+	centerOfMass[1] += velocity[1] * EngineTime::GetDeltaTime();
+	centerOfMass[2] += velocity[2] * EngineTime::GetDeltaTime();
+
+	velocity += force / mass * (float) EngineTime::GetDeltaTime();
+
+	force.set(0,0,0);
+}
+
+void AlephEngine::Kinematics::AddForce(gmtl::Vec<float, 3> addedForce)
+{
+	force += addedForce;
 }
