cc918cc9e92d9b9c93853963b546b03f3f528ba8
scottlafetra <scottlafetra@gmail.com>
2018-03-13 22:50:05 -0700

    Revert "Merge remote-tracking branch 'origin/Kinematics'"
    This reverts commit fe28a85fe9c014736e0f6490b9a29bd1e8e1e118, reversing
    changes made to ecd1ed848ef46361c3e06b606158b602761290bc.

--- AlephEngine/Kinematics.h
+++ AlephEngine/Kinematics.h
@@ -1,10 +1,6 @@
 #pragma once
 
 #include <gmtl/Vec.h>
-#include <gmtl/Quat.h>
-#include <gmtl/VecOps.h>
-
-#include "EngineTime.h"
 #include "Component.h"
 
 namespace AlephEngine
@@ -25,25 +21,12 @@ namespace AlephEngine
 	public:
 		Kinematics(Entity* entity);
 		~Kinematics();
-	private:
-		//Newtons (kg m) / s^2 Used to calculate acceleration on the object
-		gmtl::Vec<float, 3> force;
-
-	public:
-		Kinematics(Entity* entity);
-		Kinematics(Entity* entity, gmtl::Vec<float, 3> vel, gmtl::Quat<float> AV, gmtl::Vec<float, 3> CofM, float M, gmtl::Vec<float, 3> F);
 		
 		// meters/second use like vector x, y, z
-		// meters/second use like vector [index]
 		gmtl::Vec<float, 3> velocity;
-		// Rotational axis
-		gmtl::Quat<float> rotationalAxis;
-		// Relitive UP orthogonal to rotationalAxis
-		gmtl::Vec<float, 3> UP;
 		// meters Collision and gravity epicenters
 		gmtl::Vec<float, 3> centerOfMass;
 		// Kg. used for momentum, collisions, 
-		// Kg. used for momentum, collisions,
 		float mass;
 
 		// Add Force as if it was directed through the center of mass
@@ -51,8 +34,4 @@ namespace AlephEngine
 	};
 }
 
-		void MoveStep(); // Moves the entity acording to its physics
 
-		void AddForce(gmtl::Vec<float, 3> addedForce);
-	};
-}
