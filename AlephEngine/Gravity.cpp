6275027a919786af4f84340d6ff7037b29f837d8
scottlafetra <scottlafetra@gmail.com>
2018-04-01 14:12:25 -0700

    Small Gravity fixes

--- AlephEngine/Gravity.cpp
+++ AlephEngine/Gravity.cpp
@@ -1,21 +1,40 @@
 #include "Gravity.h"
+#include "Transform.h"
 
-AlephEngine::Gravity::Gravity(Kinematics& myKin) :
-	m_myKin(&myKin)
-{ }
+AlephEngine::Gravity::Gravity(Entity* entity) :
+	Component( entity, Component::Type<Gravity>())
+{ 
+	if ((myKinematics = entity->FetchComponent<Kinematics>()) == NULL)
+	{
+		FatalError("Gravity component not attached to entity with Kinematics component.");
+	}
+
+	if ((myTransform = entity->FetchComponent<Transform>()) == NULL)
+	{
+		FatalError("Gravity component not attached to entity with Kinematics component.");
+	}
+}
 
 void AlephEngine::Gravity::attract(Kinematics & yourKin)
 {
 	//distanceComponents = m_myKin - yourKin
-	gmtl::Vec<float, 3> direction = m_myKin->centerOfMass - yourKin.centerOfMass;
+	Transform* yourTransform = NULL;
+
+	if ((yourTransform = yourKin.GetEntity()->FetchComponent<Transform>()) == NULL)
+	{
+		Error(yourKin.GetEntity()->GetName() + " does not have a Transform component.");
+		return;
+	}
+
+	gmtl::Vec<float, 3> direction = (myTransform->GetPosition() + myKinematics->centerOfMass) - (yourTransform->GetPosition() + yourKin.centerOfMass);
 
 	//distance
 	float distance = sqrt(pow(direction[0], 2) + pow(direction[1], 2) + pow(direction[2], 2));
 	// force kg*m/s/s
-	float forceMagnitude = (G * m_myKin->mass * yourKin.mass / pow(distance, 2));
+	float forceMagnitude = (G * myKinematics->mass * yourKin.mass / pow(distance, 2));
 
 	gmtl::Vec<float, 3> newForce = direction / distance * forceMagnitude;
 
-	m_myKin->AddForce(newForce);
-	yourKin.AddForce(newForce * (float)-1);
+	myKinematics->AddForce(newForce);
+	yourKin.AddForce(newForce * -1.f);
 }
--- AlephEngine/Gravity.h
+++ AlephEngine/Gravity.h
@@ -5,12 +5,13 @@
 
 namespace AlephEngine
 {
-	class Gravity {
+	class Gravity : public Component{
 	public:
 		// m^3 / kg / s^2
 		const float G = 6.67 * pow(10, -11);
-		Kinematics* m_myKin;
-		Gravity(Kinematics& myKin);
+		Kinematics* myKinematics;
+		Transform* myTransform;
+		Gravity(Entity* entity);
 		void attract(Kinematics& yourKin);
 
 	};
